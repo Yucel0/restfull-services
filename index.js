fc70090270e060ae03c32cea232ccb40f27e9835
Yucel0 <yclylmz6@gmail.com>
2022-06-15 01:09:55 +0300

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const server = express();
+const cors = require("cors");
 const userRouter = require("./routers/users");
 const verifyToken = require("./middleware/verify-token");
 const port = process.env.PORT || "3000";
 
+server.use(cors());
 server.use(express.json());
 server.use("/users",verifyToken);
 server.use("/users",userRouter);
